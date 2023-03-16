92c9cf28c4bca3c5ef9601824e2c948476782791
Tommaso <92218392+TommasoScardi@users.noreply.github.com>
2023-03-16 17:21:42 +0100

    error handling implementation application now detect and handle exceptions

--- src/core/Application.php
+++ src/core/Application.php
@@ -2,26 +2,29 @@
 
 namespace MvcFramework\Core;
 
+use Exception;
+use MvcFramework\Exceptions\DbExc;
+use MvcFramework\Exceptions\HttpExc;
 use MvcFramework\Services\AppLogger;
 
 class Application
 {
+    private static AppLogger $RequestLogger;
+
     public static string $ROOT_PATH;
     public static string $SUBROOT_PATH;
-    public static AppLogger $RequestLogger;
 
     public Request $request;
     public Response $response;
     public Router $router;
 
-
     public array $services;
     
-    public function __construct($rootPath, $subPath)
+    public function __construct(string $rootPath, string $subPath, string $nameLogFile)
     {
         self::$ROOT_PATH = $rootPath;
         self::$SUBROOT_PATH = $subPath;
-        self::$RequestLogger = new AppLogger("request.log");
+        self::$RequestLogger = new AppLogger($nameLogFile);
         $this->request = new Request();
         $this->response = new Response();
         $this->router = new Router($this->request, $this->response);
@@ -34,9 +37,23 @@ class Application
 
     public function run()
     {
-        $this->router->resolve($this->services); //Fills response attribute
-        
-        http_response_code($this->response->getCode());
-        echo $this->response->getResponse();
+        try {
+            $this->router->resolve($this->services); //Fills response attribute
+        }
+        catch(DbExc $dbExc) {
+            $this->response->error(500, "DBEXCEPTION:". strval($dbExc), true, true);
+        }
+        catch(Exception $e) {
+            $this->response->error(500, $e->getMessage());
+        }
+        finally {
+            http_response_code($this->response->getCode());
+            echo $this->response->getResponse();
+        }
+    }
+
+    public static function log()
+    {
+        return self::$RequestLogger->getLogger();
     }
 }
