2aecfa1a959a3e3832913f4ad3d18f41ba37725c
Tommaso <92218392+TommasoScardi@users.noreply.github.com>
2023-03-16 17:18:49 +0100

    LOG_FILE used to determine logs name file

--- config/env.php
+++ config/env.php
@@ -2,5 +2,6 @@
 
 $dotenv = Dotenv\Dotenv::createImmutable(APP_ROOT);
 $dotenv->safeLoad();
+$dotenv->required("LOG_FILE");
 $dotenv->required(['DB_HOST', 'DB_NAME', 'DB_USER', 'DB_PWD']);
 $dotenv->required(['REDIS_HOST', 'REDIS_PWD']);
--- public/index.php
+++ public/index.php
@@ -9,7 +9,7 @@ use MvcFramework\Services\AppLogger;
 use MvcFramework\Services\DbConn;
 use MvcFramework\Services\RedisConn;
 
-$app = new Application(APP_ROOT . URL_ROOT, URL_SUBFOLDER);
+$app = new Application(APP_ROOT . URL_ROOT, URL_SUBFOLDER, $_ENV["LOG_FILE"]);
 $app->registerService("logger", new AppLogger());
 $app->registerService("db", new DbConn($_ENV["DB_HOST"], $_ENV["DB_USER"], $_ENV["DB_PWD"], $_ENV["DB_NAME"]));
 // $app->registerService("redis", new RedisConn($_ENV["REDIS_HOST"], $_ENV["REDIS_PWD"]));
