1d101289017989a12d56f48bc20b59246163cdaa
Tommaso <92218392+TommasoScardi@users.noreply.github.com>
2023-03-29 18:59:58 +0200

    context property added

--- src/core/exceptions/ServiceException.php
+++ src/core/exceptions/ServiceException.php
@@ -7,16 +7,23 @@ use Throwable;
 
 class ServiceException extends Exception
 {
-    private ?string $serviceName = null;
-    public function __construct(string $message, ?string $serviceName = null, int $code = 0, Throwable $previous = null)
+    private ?string $serviceName;
+    private array $context;
+    public function __construct(string $message, ?string $serviceName = null, int $code = 0, array $context = [], Throwable $previous = null)
     {
         parent::__construct($message, $code, $previous);
         $this->serviceName = $serviceName;
+        $this->context = $context;
     }
 
     public function getServiceName()
     {
         return $this->serviceName;
+    }
+
+    public function getContext()
+    {
+        return $this->context;
     }
 
     public function __toString()
