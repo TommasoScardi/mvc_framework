9e32ecb6d31a9abc2ec3cc5256fff26dbad63d6b
Tommaso <92218392+TommasoScardi@users.noreply.github.com>
2023-03-29 20:00:16 +0200

    implemented db find and admin user

--- src/models/User.php
+++ src/models/User.php
@@ -3,9 +3,11 @@
 namespace MvcFramework\Models;
 
 define("USER_TABLE", "users");
+define("USER_FIELD_ID", "id");
 define("USER_FIELD_NAME", "username");
 define("USER_FIELD_PASSWORD", "password");
 define("USER_FIELD_EMAIL", "email");
+define("USER_FIELD_ISADMIN", "is_admin");
 
 use Exception;
 use MvcFramework\Core\MethodReturn;
@@ -17,8 +19,9 @@ class User extends Model
     private int $id;
     private string $name;
     private string $email;
+    private bool $isAdmin;
 
-    public function __construct(int $id = 0, string $name, string $email)
+    public function __construct(int $id = 0, string $name, string $email, bool $isAdmin)
     {
         $this->id = $id;
         $this->name = $name;
@@ -27,6 +30,7 @@ class User extends Model
             throw new Exception("email was in an incorrect format");
         }
         $this->email = $email;
+        $this->isAdmin = $isAdmin;
     }
 
     /**
@@ -51,6 +55,33 @@ class User extends Model
     public function getEmail()
     {
         return $this->email;
+    }
+
+    /**
+     * Get the value of isAdmin
+     */
+    public function isAdmin()
+    {
+        return $this->isAdmin;
+    }
+
+    public static function find(DbConn $db, int|string $mailID)
+    {
+        $searchField = "";
+        if (is_numeric($mailID))
+        {
+            $searchField = USER_FIELD_ID;
+        }
+        else
+        {
+            $searchField = USER_FIELD_EMAIL;
+        }
+        $userData = $db->query(sprintf("SELECT * from %s where %s = ?", USER_TABLE, $searchField), [$mailID]);
+        if ($userData == null || count($userData) == 0)
+        {
+            return null;
+        }
+        return new User($userData[USER_FIELD_ID], $userData[USER_FIELD_NAME], $userData[USER_FIELD_EMAIL], $userData[USER_FIELD_ISADMIN]);
     }
 
     public static function login(DbConn $db, $email, string $password)
